4fc5db5c95eee68b27c2cccd30eb206648657085
Carolina Koehn <ckoehn@jeannie.fritz.box>
2025-04-07 14:40:15 +0200

    Add language files

---
+++ contao/languages/en/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+
+// contao/languages/en/modules.php
+$GLOBALS['TL_LANG']['tl_module']['lemonSqueezyApiKey'] = [
+    'Lemon Squeezy API Key', 
+    'API key from Lemon Squeezy dashboard'
+];
+
+$GLOBALS['TL_LANG']['tl_module']['lemonSqueezyManageUrl'] = [
+    'Management URL', 
+    'Base URL for subscription management (e.g. https://my.shop.lemonsqueezy.com)'
+];
+
+$GLOBALS['TL_LANG']['tl_module']['subscription'] = ['Subscription Management'];
